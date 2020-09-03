734a3c14ea1fdec358f6f721d1311b6e1452039c
Najib Daoud <najib.sdaoud@gmail.com>
2020-09-03 19:10:16 -0400

    Initial Commit

---
+++ Tip App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip App
+//
+//  Created by Najib Daoud on 9/3/20.
+//  Copyright © 2020 Najib Daoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
